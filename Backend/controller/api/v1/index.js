b2e1e2c98bc45c025b087dcf16f778fb8f2ec34b
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2024-02-15 16:08:06 +0000

    install axios

--- Backend/controller/api/v1/index.js
+++ Backend/controller/api/v1/index.js
@@ -1,3 +1,16 @@
+const axios = require('axios');
+let database = [];
 module.exports.products_trancations = async (req,res) => {
-    return res.send("my server");
+    const urlToFetch = 'https://s3.amazonaws.com/roxiler.com/product_transaction.json';
+   
+    axios.get(urlToFetch)
+      .then(response => {
+       database.push(response.data);
+       console.log(database)
+        // Process the response data here
+      })
+      .catch(error => {
+        console.error('Error fetching URL:', error.message);
+        // Handle errors here
+      });
 }
